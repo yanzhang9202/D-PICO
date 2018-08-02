fae8e7fc40a5ef2ab4e10e08e4707095a6a667d4
yanzhang9202 <yan.zhang9202@gmail.com>
2018-08-01 22:42:48 -0400

    framework added

--- gen_randQP.m
+++ gen_randQP.m
@@ -1,1 +1,9 @@
 %% Create random QP problems
+N = 5;  % number of the agents
+n = 2;
+m = 1;
+
+% Generate data
+Q = zeros(n,n,N);
+q = zeros(n,N);
+A = zeros(m,n,N);
--- main.m
+++ main.m
@@ -9,3 +9,22 @@ rnd_seed = 2;
 rng(rnd_seed);
 
 gen_problem;
+
+% Algorithms
+ind_alg = 1;    % 1 - matlab;   2 - DD;     3 - C-DD
+                % 4 - PIC-DD;   5 - EC-DD;
+
+switch ind_alg
+    case 1
+        main_matlab;
+    case 2
+        main_dd;
+    case 3
+        main_cdd;
+    case 4
+        main_picdd;
+    case 5
+        main_ecdd;
+    otherwise
+        error('Undefined algorithm!')
+end
