c9056ac7be810ef98c6251107ddf00931575d632
Yan Zhang <yan.zhang9202@gmail.com>
2018-08-02 11:20:55 -0400

    matlab solver added

--- gen_randQP.m
+++ gen_randQP.m
@@ -5,5 +5,10 @@ m = 1;
 
 % Generate data
 Q = rand(n,n,N);
+for ii = 1 : N
+   Q(:,:,ii) = Q(:,:,ii)'*Q(:,:,ii);    % Make Qi matrices (semi)positive definite
+end
 q = rand(n,N);
 A = rand(m,n,N);
+l = -ones(n, N);
+u = ones(n, N);
