9843eb5c6af26be52b699c94f5105d75831d55b2
Yan Zhang <yan.zhang9202@gmail.com>
2018-08-02 16:38:19 -0400

    lambda plot function added

--- solver/dd/main_dd.m
+++ solver/dd/main_dd.m
@@ -5,24 +5,26 @@ step = 1e-2;
 eps_dd = 1e-2;
 
 % initial var.
-lambda_itr = zeros(m, 1);
+lambda = zeros(m, 1);
+lambda_itr = zeros(m, iter_max);
 x_itr = zeros(n, iter_max, N);
 Add = datam.A;
 
 % Run
 fprintf('DD solver starts... \n')
 for ii = 1 : iter_max
+    lambda_itr(:,ii) = lambda;
     % Solve local problems
     for jj = 1 : N
         x_itr(:,ii,jj) = locfun_dd(Q(:,:,jj),q(:,jj),A(:,:,jj),...
-            l(:,jj),u(:,jj),lambda_itr);
+            l(:,jj),u(:,jj),lambda);
     end
     % Update the multiplier
     rsd = Add*reshape(x_itr(:,ii,:),[],1);
     if norm(rsd) < eps_dd
         break;
     end
-    lambda_itr = lambda_itr + step*rsd;
+    lambda = lambda + step*rsd;
     if mod(ii, 100) == 1
         fprintf(['The ', num2str(ii), ' th iter. done. The rsd is ', ...
             num2str(norm(rsd)), '!\n'])
@@ -33,10 +35,11 @@ end
 soldd.x = x_itr(:, ii, :);
 soldd.x = soldd.x(:);
 soldd.x_itr = x_itr;
-soldd.lambda = lambda_itr;
+soldd.lambda = lambda;
+soldd.lambda_itr = lambda_itr;
 soldd.num_itr = ii;
 soldd.step = step;
 soldd.eps = eps_dd;
+sol{ind_alg} = soldd;
 
-clear lambda_itr x_itr Add iter_max step eps_dd
-
+clear lambda lambda_itr x_itr Add iter_max step eps_dd soldd
